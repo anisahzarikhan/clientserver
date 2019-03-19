463950fd563b92248a4f8ab9270bc6d8a6cdb09d
anisahzarikhan <48428773+anisahzarikhan@users.noreply.github.com>
2019-03-20 02:06:39 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,83 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <netdb.h>
-#include <netinet/in.h>
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
 #include <string.h>
 #include <unistd.h>
-
-char enterChar();
-int main(int argc, char *argv[]) 
-{
-   int sockfd, portno, n;
-   struct sockaddr_in serv_addr;
-   struct hostent *server;
+#define PORT 8080 
    
-   char buffer[256];
+int main(int argc, char const *argv[]) 
+{ 
+    struct sockaddr_in address; 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hi = "What's Up from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
    
-   if (argc < 3) 
-   {
-      fprintf(stderr,"usage %s hostname port\n", argv[0]);
-      exit(0);
-   }
-	
-   portno = atoi(argv[2]);
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
    
-   /* Create a socket point */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
    
-   if (sockfd < 0) 
-   {
-      perror("ERROR opening socket");
-      exit(1);
-   }
-	
-   server = gethostbyname(argv[1]);
-   
-   if (server == NULL) 
-   {
-      fprintf(stderr,"ERROR, no such host\n");
-      exit(0);
-   }
-   
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(portno);
-   
-   /* Now connect to the server */
-   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
-   {
-      perror("ERROR connecting");
-      exit(1);
-   }
-   
-   /* Now ask for a message from the user, this message
-      * will be read by server
-   */
-	
-   printf("Please enter the message: ");
-   bzero(buffer,256);
-   fgets(buffer,255,stdin);
-   
-   /* Send message to the server */
-   n = write(sockfd, buffer, strlen(buffer));
-   
-   if (n < 0) 
-   {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
-   
-   /* Now read server response */
-   bzero(buffer,256);
-   n = read(sockfd, buffer, 255);
-   
-   if (n < 0) 
-   {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
-	
-   printf("What's Up?",buffer);
-   return 0;
-}
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    send(sock , hi , strlen(hi) , 0 ); 
+    printf("What's Up?\n"); 
+    valread = read( sock , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    return 0; 
+} 
