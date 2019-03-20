cfd18622e7485c6e1b2dae60cba2cf983d1aa2d5
anisahzarikhan <48428773+anisahzarikhan@users.noreply.github.com>
2019-03-20 08:29:49 +0800

    Update client.c

--- client.c
+++ client.c
@@ -8,27 +8,7 @@
 #define MAX 80 
 #define PORT 8080 
 #define SA struct sockaddr 
-void func(int sockfd) 
-{ 
-    char buff[MAX+1]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
-  
+
 int main() 
 { 
     int sockfd, connfd; 
@@ -36,7 +16,8 @@ int main()
   
     // socket create and varification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
+    if (sockfd == -1) 
+    { 
         printf("socket creation failed...\n"); 
         exit(0); 
     } 
@@ -50,7 +31,8 @@ int main()
     servaddr.sin_port = htons(PORT); 
   
     // connect the client socket to server socket 
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) 
+    { 
         printf("connection with the server failed...\n"); 
         exit(0); 
     } 
