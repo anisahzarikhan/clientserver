5e47aeddf3045f7c0b3c3df9fed9a6c2aea668b5
anisahzarikhan <48428773+anisahzarikhan@users.noreply.github.com>
2019-04-07 21:49:18 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,44 +1,81 @@
-#include <netdb.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#define PORT 8080 
-#define SA struct sockaddr 
+int main(int argc, char *argv[])
+{
+	int fd = 0;
+	char buff[1024];
+	
+	if(argc<3)
+	{
+		printf("Less no of arguments !!");
+		return 0;
+	}
 
-int main() 
-{ 
-    int sockfd; 
-    struct sockaddr_in servaddr; 
-  
-    // socket create and varification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) 
-    { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n");
-        printf("What's Up!\n");
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("192.168.81.130"); 
-    servaddr.sin_port = htons(PORT); 
-  
-    // connect the client socket to server socket 
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) 
-    {  
-        printf("connection failed..\n");
-        exit(0); 
-    } 
-    else
-        printf("connected to the server..\n");
-        printf("Howdy!..\n");
-        exit(0);
-  
-} 
+	//Setup Buffer Array
+	memset(buff, '0',sizeof(buff));	
+
+	//Create Socket
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+    	if(fd<0)
+	{
+		perror("Client Error: Socket not created succesfully");
+		return 0;
+	}
+	
+	//Structure to store details
+	struct sockaddr_in server; 
+	memset(&server, '0', sizeof(server)); 
+
+	//Initialize	
+	server.sin_family = AF_INET;
+	server.sin_port =  htons(atoi(argv[2]));
+
+	int in = inet_pton(AF_INET, argv[1], &server.sin_addr);
+	if(in<0)
+	{
+		perror("Client Error: IP not initialized succesfully");
+		return 0;
+	}
+
+	//Connect to given server	
+	in = connect(fd, (struct sockaddr *)&server, sizeof(server));
+	if(in<0)
+	{
+		perror("Client Error: Connection Failed.");
+		return 0;
+	}    	
+
+	while(1)
+	{
+		printf("Please enter the message: ");
+    		bzero(buff,256);
+    		fgets(buff,255,stdin);
+    		
+		    printf("\nSending to SERVER: %s ",buff);
+	
+		/* Send message to the server */
+    		in = send(fd,buff,strlen(buff),0);
+		    if (in < 0) 
+		    {
+			 perror("\nClient Error: Writing to Server");
+		    	return 0;
+		    }
+		    
+		/* Now read server response */
+		    bzero(buff,256);
+		    in = recv(fd,buff,255,0);
+		    if (in < 0) 
+		    {
+			 perror("\nClient Error: Reading from Server");
+			return 0;
+		    }
+		    printf("\nReceived FROM SERVER: %s ",buff);
+	}
+	close(fd);
+	return 0;
+}
