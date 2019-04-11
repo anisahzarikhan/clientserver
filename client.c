7e8fdf697633748f465c7aef44eb788a615ee839
anisahzarikhan <48428773+anisahzarikhan@users.noreply.github.com>
2019-04-11 14:08:28 +0800

    Update client.c

--- client.c
+++ client.c
@@ -7,73 +7,57 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-void error(const char *msg)
+void error(const char *message)
 {
-	perror(msg);
-	exit(0);
+    perror(message);
+    exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-    
-    int server_port, socket_fd, n;
-    struct hostent *server_host;
-    struct sockaddr_in server_address;
-    char buff[333];
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-    /* Get server hostname port */
-    if (argc < 3) 
-    {
-        printf(stderr "user %s hostname port", argv[0]);
-        exit(0);
-        
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
     }
-
-    /* Get server port from command line arguments or stdin. */
-    server_port = atoi(argv[2]);
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0)
-    error("socket failed");
-    
-    /* Get server host from server name. */
-    server_host = gethostbyname(argv[1]);
-    if (server_host == NULL)
-    {
-        printf(stderr, "Error, no host");
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-
-    /* Initialise IPv4 server address with server host. */
-    bzero((char *) &server_address, sizeof (server_address));
-    server_address.sin_family = AF_INET;
-    bcopy((char *)server_host->h_addr,
-          (char *) &server_address.sin_addr.s_addr,
-          server_host->h_length); 
-          
-    server_address.sin_port = htons(server_port);
-
-    /* Connect to socket with server address. */
-    if (connect(socket_fd, (struct sockaddr *)&server_address, sizeof server_address)<0) 
-		perror("connect error");
-		printf("Client: ");
-        while(1);
-	{
-
-    bzero(buff,333);
-        fgets(buff,333,stdin);
-        n = write(socket_fd,buff,strlen(buff));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Client: ");
+    while(1)
+    {
+        bzero(buffer,255);
+        fgets(buffer,255,stdin);
+        n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
-             error("socket write error");
-        bzero(buff,333);
-        n = read(socket_fd,buff,333);
+             error("ERROR writing to socket");
+        bzero(buffer,255);
+        n = read(sockfd,buffer,255);
         if (n < 0) 
-             error("socket read error");
-        printf("Server : %s\n",buff);
-        int i = strncmp("Assalammualaikum..." , buff , 3);
+             error("ERROR reading from socket");
+        printf("Server : %s\n",buffer);
+        int i = strncmp("Bye" , buffer , 3);
         if(i == 0)
                break;
-	}
-    
-    close(socket_fd);
+    }
+    close(sockfd);
     return 0;
 }
