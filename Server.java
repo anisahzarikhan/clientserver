6781b1a203bae056fbd7e589fd460807392c24c4
anisahzarikhan <48428773+anisahzarikhan@users.noreply.github.com>
2019-04-11 08:45:07 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,86 +1,60 @@
-import java.io.*;
 import java.net.*;
+import java.io.*;
 import java.lang.*;
+import java.util.*;
+class Server
+{
 
-public class Server 
-
-{
-        
-	public static void main(String[] args) throws IOException 
-	{
-		final int port = 8080;
-		System.out.println("Server waiting for connection on port "+port);
-		ServerSocket ss = new ServerSocket(port);
-		Socket clientSocket = ss.accept();
-		System.out.println("Recieved connection from "+clientSocket.getInetAddress()+" on port "+clientSocket.getPort());
-		//create two threads to send and recieve from client
-		RecieveFromClientThread recieve = new RecieveFromClientThread(clientSocket);
-		Thread thread = new Thread(recieve);
-		thread.start();
-		SendToClientThread send = new SendToClientThread(clientSocket);
-		Thread thread2 = new Thread(send);
-		thread2.start();
-	}}
-class RecieveFromClientThread implements Runnable
-{
-	Socket clientSocket=null;
-	BufferedReader brBufferedReader = null;
-	
-	public RecieveFromClientThread(Socket clientSocket)
-	{
-		this.clientSocket = clientSocket;
-	}//end constructor
-	public void run() 
-	{
-		try
-		{
-		brBufferedReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));		
-		
-		String messageString;
-		while(true)
-		{
-		while((messageString = brBufferedReader.readLine())!= null){//assign message from client to messageString
-			if(messageString.equals("EXIT"))
-			{
-				break;//break to close socket if EXIT
-			}
-			System.out.println("From Client: " + messageString);//print the message from client
-			System.out.println("Server: ");
-		}
-		this.clientSocket.close();
-		System.exit(0);
-	}
-		
-	}
-	catch(Exception ex){System.out.println(ex.getMessage());}
-	}
-}//end class RecieveFromClientThread
-class SendToClientThread implements Runnable
-{
-	PrintWriter pwPrintWriter;
-	Socket clientSock = null;
-	
-	public SendToClientThread(Socket clientSock)
-	{
-		this.clientSock = clientSock;
-	}
-	public void run() 
-	{
-		try{
-		pwPrintWriter =new PrintWriter(new OutputStreamWriter(this.clientSock.getOutputStream()));//get outputstream
-		
-		while(true)
-		{
-			String msgToClientString = null;
-			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));//get userinput
-			
-			msgToClientString = input.readLine();//get message to send to client
-			
-			pwPrintWriter.println(msgToClientString);//send message to client with PrintWriter
-			pwPrintWriter.flush();//flush the PrintWriter
-			System.out.println("server: ");
-		}//end while
-		}
-		catch(Exception ex){System.out.println(ex.getMessage());}	
-	}//end run
+    public static void main (String[] args)
+    {
+      try{      
+      //Defining/opening connection
+      ServerSocket srvr = new ServerSocket(8080);
+      Socket skt = srvr.accept();
+      
+      InputStreamReader bf = new InputStreamReader(skt.getInputStream());
+      BufferedReader in= new BufferedReader(bf);
+      PrintWriter out = new PrintWriter(skt.getOutputStream(),true);
+      Scanner s=new Scanner(System.in);
+      
+      Thread send=new Thread(new Runnable(){
+      String msg;
+      @Override
+      public void run(){
+            while(true){
+  
+                  msg=s.nextLine();
+                  out.print(msg);
+                  out.flush();
+            }
+      }
+      });
+      send.start();
+      
+      Thread receive=new Thread(new Runnable(){
+      String msg;
+      @Override
+      public void run(){
+            while(true){
+                  try{
+                        msg=in.readLine();
+                  }catch(IOException e){
+                        e.printStackTrace();
+                  }
+                  System.out.print("Client: "+msg);
+            }
+      }
+      });
+      receive.start();
+            
+      
+      
+      //closing connection
+      //out.close();
+      //skt.close();
+      //srvr.close();
+      }catch (IOException e){
+            e.printStackTrace();
+      }
+    }
 }
