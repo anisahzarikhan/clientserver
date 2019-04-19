992c16b891cdbdf5388c4ff2db977b53fdc3ea2e
anisahzarikhan <48428773+anisahzarikhan@users.noreply.github.com>
2019-04-19 22:19:25 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,61 +1,32 @@
-import java.net.*; 
-import java.io.*; 
-import java.util.*;  
+import java.io.*;
+import java.net.*;
+import java.lang.*;
 public class Server 
-{ 
-    //initialize socket and input stream 
-    private Socket          socket   = null; 
-    private ServerSocket    server   = null; 
-    private DataInputStream in       =  null; 
-  
-    // constructor with port 
-    public Server(int port) 
-    { 
-        // starts server and waits for a connection 
-        try
-        { 
-            Server = new ServerSocket(port); 
-            System.out.println("Server started"); 
-  
-            System.out.println("Waiting for a client ..."); 
-  
-            socket = server.accept(); 
-            System.out.println("Client accepted"); 
-  
-            // takes input from the client socket 
-            in = new DataInputStream( 
-                new BufferedInputStream(socket.getInputStream())); 
-  
-            String line = ""; 
-  
-            // reads message from client until "Over" is sent 
-            while (!line.equals("Over")) 
-            { 
-                try
-                { 
-                    line = in.readUTF(); 
-                    System.out.println(line); 
-  
-                } 
-                catch(IOException i) 
-                { 
-                    System.out.println(i); 
-                } 
-            } 
-            System.out.println("Closing connection"); 
-  
-            // close connection 
-            socket.close(); 
-            in.close(); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        Server server = new Server(4444); 
-    } 
-} 
+{
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("Server waiting for connection from client");
+		ServerSocket ss = new ServerSocket(4444);
+		Socket sock = ss.accept();
+		
+		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                      
+                OutputStream ostream = sock.getOutputStream(); 
+                PrintWriter pwriter = new PrintWriter(ostream, true);
+
+                InputStream istream = sock.getInputStream();
+                BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+
+      String receiveMessage, sendMessage;               
+      while(true)
+      {
+        if((receiveMessage = receiveRead.readLine()) != null)  
+        {
+           System.out.println(receiveMessage);         
+        }         
+        sendMessage = keyRead.readLine(); 
+        pwriter.println(sendMessage);             
+        pwriter.flush();
+      }               
+    }                    
+}                        
