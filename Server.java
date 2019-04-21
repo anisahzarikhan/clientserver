da8623134bb3e5307bbef307685dffb710b91f4b
anisahzarikhan <48428773+anisahzarikhan@users.noreply.github.com>
2019-04-21 22:02:12 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,32 +1,33 @@
-import java.io.*;
-import java.net.*;
-import java.lang.*;
+package server_sock;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class Server 
 {
-	public static void main(String[] args) throws Exception
-	{
-		System.out.println("Server waiting for connection from client");
-		ServerSocket ss = new ServerSocket(4444);
-		Socket sock = ss.accept();
-		
-		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-	                      
-                OutputStream ostream = sock.getOutputStream(); 
-                PrintWriter pwriter = new PrintWriter(ostream, true);
 
-                InputStream istream = sock.getInputStream();
-                BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
-      String receiveMessage, sendMessage;               
-      while(true)
-      {
-        if((receiveMessage = receiveRead.readLine()) != null)  
-        {
-           System.out.println(receiveMessage);         
-        }         
-        sendMessage = keyRead.readLine(); 
-        pwriter.println(sendMessage);             
-        pwriter.flush();
-      }               
-    }                    
-}                        
+    public static void main(String[] args) 
+    {
+
+try
+{
+    ServerSocket ser = new ServerSocket (4444);
+    Socket sock = ser.accept();
+
+    BufferedReader ed =new BufferedReader (new InputStreamReader(sock.getInputsStream()));
+    String tmp = ed.readLine();
+    System.out.print("Server:"+tmp);
+
+    PrintStream pr = new PrintStream(sock.getOutputStream());
+    String str = "The message delivered succesfully !!"
+    pr.println(str);
+    }
+    catch(Exception ex){}
+
+    }
+}
